6509b2b88475426d092908d0bc446081b8d84abe
majh <majh@cunxin.com>
2023-02-07 19:55:57 +0800

    update

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,12 +1,16 @@
 import { Button } from "ui";
-import {} from "@shirtiny/utils/src/main";
-import {} from "@shirtiny/logger/src/main";
+import { math } from "@shirtiny/utils/src/main";
+import logger from "@shirtiny/logger/src/main";
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
-      <Button />
+      <Button
+        onClick={() => {
+          logger.debug("Web", "Button clicked", math.restrict(1, 2, 3));
+        }}
+      />
     </div>
   );
 }
