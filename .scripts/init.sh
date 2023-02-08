e0a79ba31e747849a325512eb8b1ed20c1fc01e3
majh <majh@cunxin.com>
2023-02-09 00:24:45 +0800

    update sh

--- .scripts/init.sh
+++ .scripts/init.sh
@@ -5,4 +5,8 @@ set -eu
 # git submodule init; git submodule update --recursive
 git submodule update --init --recursive
 
+cd ./packages/core && git checkout main
+cd ../packages/utils && git checkout main
+cd ../../
+
 yarn
